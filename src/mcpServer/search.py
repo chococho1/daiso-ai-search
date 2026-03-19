285ad0acf937d6dbb2e27b9b506bb96374fe7e82
chococho1 <chococho1@daiso.co.kr>
2026-03-19 13:54:15 +0900

    2026.03.19 : 데모 프롬프트 작성

--- src/mcpServer/search.py
+++ src/mcpServer/search.py
@@ -1,4 +1,5 @@
 from mcp.server.fastmcp import FastMCP
+from prompt.search_prompt import TOOL_DESCRIPTIONS
 import requests
 
 
@@ -7,9 +8,6 @@ mcp = FastMCP("--- DaisoMall Search Mcp ---")
 
 @mcp.tool()
 def prod_search(query: str) -> list:
-  """ DaisoMall Prod Search (다이소몰 상품 통합검색)
-    query: 검색할 상품명 또는 키워드 또는 품번
-  """
   url = f"https://prdm.daisomall.co.kr/ssn/search/SearchGoods?searchTerm={query}"
 
   try:
@@ -22,14 +20,30 @@ def prod_search(query: str) -> list:
 
     if len(results) > 0:
       product_list = results[1].get("resultDocuments", [])
-      return product_list
+      final_results = []
+      for product in product_list:
+        pdNo = product.get("pdNo")
+        exhPdNm = product.get("exhPdNm")
+        pdPrc = product.get("pdPrc")
+
+        # 상품상세 url
+        prod_detail_url = f"https://www.daisomall.co.kr/pd/pdr/SCR_PDR_0001?pdNo={pdNo}&recmYn=N"
+
+        #필요한 정보만 넣기
+        final_results.append({
+          "상품명" : exhPdNm,
+          "품번" : pdNo,
+          "가격" : f"{pdPrc}원",
+          "링크" : f"[상품상세]({prod_detail_url})"
+        })
+      return final_results
     else:
       return []
   except  Exception as e:
     print("에러났음!!  -- {e}")
   return
 
-
+prod_search.__doc__ = TOOL_DESCRIPTIONS["prod_search"]
 
 @mcp.resource("greeting://{name}")
 def get_greeting(name: str) -> str:
